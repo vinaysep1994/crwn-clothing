ffa6dd3eb97f806c06eb8282c3eed6ca8da12455
vinaysep1994 <vinay99.nalanda@gamil.com>
2022-02-02 23:06:19 +0530

    Stripe payment integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem  from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -33,7 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
   <CheckoutItem key={cartItem.id} cartItem={cartItem} />
 ))}
     <div className='total'>TOTAL: ${total}</div>
-  </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*<br/>
+      4242 4242 4242 4242 4242 -EXP:01/25-CVV:123
+    </div>
+    <StripeCheckoutButton price={total}/>
+    </div>
 );
 
 const mapStateToProps = createStructuredSelector({
